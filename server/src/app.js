ed6a51376be51a18afa03c308aff32503f2b5db1
DeveshBhagwani <deveshzindahai@gmail.com>
2026-02-03 06:16:12 +0530

    fix vercel SPA routing

--- server/src/app.js
+++ server/src/app.js
@@ -1,37 +1,42 @@
 import express from "express";
 import cors from "cors";
 import authRoutes from "./routes/auth.routes.js";
-import errorHandler from "./middleware/error.middleware.js";
 import postRoutes from "./routes/post.routes.js";
 import analyticsRoutes from "./routes/analytics.routes.js";
 import userRoutes from "./routes/user.routes.js";
+import errorHandler from "./middleware/error.middleware.js";
+
 const app = express();
+
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://social-pulse-beryl.vercel.app/"
+  "https://social-pulse-beryl.vercel.app"
 ];
 
-app.use(cors({
+const corsOptions = {
   origin: function (origin, callback) {
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
-    } else {
+    } 
+    else {
       callback(new Error("CORS not allowed"));
     }
   },
   credentials: true,
-  allowedHeaders: ["Content-Type", "Authorization"],
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
-}));
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+};
 
-app.options("*", cors());
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions)); 
+
 app.use(express.json());
-// Routes FIRST
+
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/analytics", analyticsRoutes);
-// Error middleware LAST (always)
+
 app.use(errorHandler);
 
 export default app;
